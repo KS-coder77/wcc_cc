c51c5ca3bede5eb0d0d1660e194ce97db18a0414
Nidhi Yadav <xs2nidhi@gmail.com>
2024-12-30 20:03:56 +0100

    Updated test for Quiz rendering when level is passed

--- src/tests/quiz.test.js
+++ src/tests/quiz.test.js
@@ -8,20 +8,22 @@ jest.mock('../components/score/ScoreCard', () => {
         return <div>Mocked ScoreCard</div>;
       };
   });
-  questions: [
-    { id: 1,question: 'What is 2 + 2?', answer: '4' },
-    { id:2, question: 'What is 3 + 5?', answer: '8' },
-  ],
+  
 describe('Quiz Component correctly renders', () => {
-    const mockResetQuiz = jest.fn(); // Mock the resetQuiz function
-    const mockHandleSubmit = jest.fn(); // Mock the handleSubmit function
+  const questions = [
+    { id: 1, question: 'What is 2 + 2?', answer: '4' },
+    { id: 2, question: 'What is 3 + 5?', answer: '8' },
+  ];
+
+  const mockResetQuiz = jest.fn(); // Mock the resetQuiz function
+  const mockHandleSubmit = jest.fn(); // Mock the handleSubmit function
 
       const defaultProps = {
         showScore: false,
         score: 7,
-        questions: Array(10).fill({}),
-        currentQuestion: 10,
-        question: "What is the square of 4?",
+       questions: Array(10).fill({}),
+       currentQuestion: 1,
+       question: "What is the square of 4?",
         userInput: "",
         setUserInput: jest.fn(),
         handleSubmit: mockHandleSubmit,
@@ -30,14 +32,26 @@ describe('Quiz Component correctly renders', () => {
 
     // it('renders the quiz card when showScore is false', () => {
     //     //render(<Quiz {...defaultProps} />);
-    //       render(<Quiz {...defaultProps} level={""} questions={questions}/>);
+    //       render(<Quiz questions={questions} level={"Easy"}/>);
     //     // Ensure the quiz card renders
     //     expect(screen.getByText(/Math Master Quiz 🧮/i)).toBeInTheDocument();
-    //     expect(screen.getByText(/Question 1\/10/i)).toBeInTheDocument();
-    //     expect(screen.getByText(/What is the square of 4?/i)).toBeInTheDocument();
+    //     expect(screen.getByText(/What is 2 + 2?/i)).toBeInTheDocument();
     //     expect(screen.getByLabelText(/your answer/i)).toBeInTheDocument();
     //     expect(screen.getByRole('button', { name: /submit/i })).toBeDisabled();
     // });
+    const questionsMed = [
+      { id: 1, question: 'What is 2 *2?', answer: '4' },
+      { id: 2, question: 'What is 3 * 5?', answer: '8' },
+    ];
+    it('renders the quiz card when showScore is false', () => {
+      //render(<Quiz {...defaultProps} />);
+        render(<Quiz questions={questionsMed} level={"Medium"}/>);
+      // Ensure the quiz card renders
+      expect(screen.getByText(/Math Master Quiz 🧮/i)).toBeInTheDocument();
+      //expect(screen.getByText(/What is 2 * 2?/i)).toBeInTheDocument();
+      expect(screen.getByLabelText(/your answer/i)).toBeInTheDocument();
+      expect(screen.getByRole('button', { name: /submit/i })).toBeDisabled();
+  });
     it('enables input button when input is provided',()=>{
         render(<Quiz{...defaultProps} userInput="16"/>)
         const submitButton=screen.getByRole('button',{name:/submit/i});
@@ -55,8 +69,8 @@ describe('Quiz Component correctly renders', () => {
         const submitButton = screen.getByRole('button', { name: /submit/i });
         fireEvent.click(submitButton);
         screen.debug;
-        //render(<ScoreCard/>);
-       // expect(screen.getByText('/ScoreCard/i')).toBeInTheDocument();
+      //   render(<ScoreCard/>);
+      //  expect(screen.getByText('/ScoreCard/i')).toBeInTheDocument();
     });
    
 });
